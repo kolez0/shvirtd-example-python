8507a2323e7df6d294e8872363abf1e86edede7e
kolez0 <91115639+kolez0@users.noreply.github.com>
2026-05-26 20:44:18 +0300

    add script.sh

---
+++ script.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/bash
+
+FOLDER="/opt/shvirtd-example-python"
+REPO="https://github.com/kolez0/shvirtd-example-python.git"
+
+if [ -d "$FOLDER" ]; then
+    sudo rm -rf "$FOLDER"
+fi
+
+sudo git clone "$REPO" "$FOLDER"
+
+cd "$FOLDER"
+
+docker compose up -d
